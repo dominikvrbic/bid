f69315f09525fe1cdd4a720fbb1a521c1f7edfce
Dominik Vrbic <dominik.vrbic@netteam.com.hr>
2019-08-07 23:06:46 +0200

    Socket class, example

--- backend/index.js
+++ backend/index.js
@@ -44,7 +44,16 @@ app.get('/', (req, res) => {
     res.send('Alo bre');
 });
 
+function wsSend(socket, type, data) {
+    const msg = {type, data};
+    socket.send(JSON.stringify(msg));
+}
+
 app.ws('/socket', (ws, req) => {
+    setInterval(() => {
+        wsSend(ws, 'priceChange', {price: 100, imageId: 123});
+    }, 2000);
+
     ws.on('message', msg => {
         console.log('Got sg: ', msg);
         ws.send(msg);
