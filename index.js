70d351e04c991bdd1a9908abfd89c3792bc12a15
Dominik Vrbic <dominik.vrbic@netteam.com.hr>
2019-08-07 22:20:33 +0200

    Add nodemon and start/dev npm scripts

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const express = require('express');
+const enableWs = require('express-ws');
+
 const Sequelize = require('sequelize');
 const config = require('./config/database.json')['development'];
 
@@ -32,4 +35,24 @@ async function tryLogin(email, password) {
 //     email: 'dvrbic@gmail.com',
 // });
 
-tryLogin('dvrbic@gmail.com', 'blabla').then(user => console.log('User: ', user));
+// tryLogin('dvrbic@gmail.com', 'blabla').then(user => console.log('User: ', user));
+
+const app = express();
+enableWs(app);
+
+app.get('/', (req, res) => {
+    res.send('Alo bre');
+});
+
+app.ws('/socket', (ws, req) => {
+    ws.on('message', msg => {
+        console.log('Got sg: ', msg);
+        ws.send(msg);
+    });
+
+    ws.on('close', () => {
+        console.log('WebSocket was closed')
+    });
+});
+
+app.listen(8000);
