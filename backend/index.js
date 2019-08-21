826dd3246b268dd77ab681e5ab28afb1e24e5c92
Dominik Vrbic <dominik.vrbic@netteam.com.hr>
2019-08-21 08:34:12 +0200

    dodo funkcije

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,9 @@ const enableWs = require('express-ws');
 const bodyParser = require('body-parser');
 const Sequelize = require('sequelize');
 const config = require('./config/database.json')['development'];
+const Bid = require('./db/models/bid');
+const Picture = require('./db/models/picture');
+
 
 console.log('Config? ', config);
 const sequelize = new Sequelize(config);
@@ -49,7 +52,66 @@ app.post('/register', (req, res) => {
         message: `${req.body.email}user was reged`
     })
 
-})
+});
+
+
+app.get('/bidup/:id', (req, res) => {
+    let bidID = req.params.id;
+    let currentBid = 0;
+    let x = 10;
+    Bid.findOne({
+        attributes: ['price'],
+        where: {id: bidID}
+    })
+    .then(bid => {
+        currentBid = bid[0].price;
+    })
+    .catch(err => res.send({
+        message: `nemrem najti id u bazi  : ${err}`
+    }));
+    Bid.update({
+        price: currentBid + x,
+        
+    }, {
+        where: {bidID: id}
+    })
+    .then(res.send)({
+        message: 'bid je uspjesan'
+    }) 
+    .catch(err => res.send({
+        message: `nekaj ne valja :${err}`
+    }));
+});
+
+//sve slike url, title i photographer
+app.get('/sveslike', (req, res) => {
+    Picture.findAll({
+        attributes: ['url', 'title', 'photographer', 'id']
+    })
+    .then(pictures => {
+        res.send(JSON.stringify({"response": pictures}));
+    })
+    .catch(err => res.send({
+        message: `nekaj ne valja :${err}`
+    }));
+});
+
+//specificna slika
+app.get('/slika/:id', (req, res) => {
+    let bidID = req.params.id;
+    Picture.findOne({
+       where: {
+           id: bidID
+       } 
+    })
+    .then(picture => {
+        res.send(JSON.stringify({"response": picture}));
+    })
+    .catch(err => res.send({
+        message: `nekaj ne valja :${err}`
+    }));
+});
+
 function wsSend(socket, type, data) {
     const msg = { type, data };
     socket.send(JSON.stringify(msg));
